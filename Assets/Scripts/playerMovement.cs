86b7f8ce16486c65edfa8c820548d58818b4a206
Lily <70020377+lily5463@users.noreply.github.com>
2024-12-17 16:43:26 +0700

    Add movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
 
 //initialization
+float maxSpeed = 3.0f;
 void Start() {
 
 }
 
-void FixedUpdate() {
+void Update() {
+    // return a FLOAT from -1.0 to +1.0
+    
+    Vector3 pos = transform.position;
 
+    pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+    // pos.y += Input.GetAxis("Vertical");
+    transform.position = pos;
+    // transform.position.y = 
 }
-
 }
 
 
