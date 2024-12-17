e3ac08c25e9cd22370351fb4de0c72832d9f44df
Lily <70020377+lily5463@users.noreply.github.com>
2024-12-17 23:19:56 +0700

    Update playerMovement.cs

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,23 +1,64 @@
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour {
+public class playerMovement : MonoBehaviour
+{
 
-//initialization
-float maxSpeed = 3.0f;
-void Start() {
+    float maxSpeed = 5f;
+    float rotSpeed = 180f;
+    float shipBoundaryRadius = 0.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        //ROTATE THE SHIP
+
+        //Grab our rotation quaternion
+        Quaternion rot = transform.rotation;
+
+        //Grab the Z euler angle
+        float z = rot.eulerAngles.z;
+
+        //Change the Z angle based on input
+        z -= Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+
+        //Recreate the quaternion
+        rot = Quaternion.Euler( 0, 0, z );
+
+        //Feed the quaternion into our rotation
+        transform.rotation = rot;
+
+        //MOVE THE SHIP
+        Vector3 pos = transform.position;
+
+        Vector3 velocity = new Vector3( 0, Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime, 0 );
+
+        pos += rot * velocity;
+
+        if (pos.y + shipBoundaryRadius > Camera.main.orthographicSize) {
+            pos.y = Camera.main.orthographicSize - shipBoundaryRadius;
+        }
+        if (pos.y - shipBoundaryRadius < - Camera.main.orthographicSize) {
+            pos.y = Camera.main.orthographicSize + shipBoundaryRadius;
+        }
+
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float widthOrtho = Camera.main.orthographicSize * screenRatio;
+
+        if (pos.x + shipBoundaryRadius > Camera.main.orthographicSize) {
+            pos.x = widthOrtho - shipBoundaryRadius;
+        }
+        if (pos.x - shipBoundaryRadius < - Camera.main.orthographicSize) {
+            pos.x = - widthOrtho + shipBoundaryRadius;
+        }
+
+        //Update our position
+        transform.position = pos;
+
+
+    }
 }
-
-void Update() {
-    // return a FLOAT from -1.0 to +1.0
-    
-    Vector3 pos = transform.position;
-
-    pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
-    // pos.y += Input.GetAxis("Vertical");
-    transform.position = pos;
-    // transform.position.y = 
-}
-}
-
-
